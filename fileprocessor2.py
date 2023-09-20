c1430891abe79ea83df762b483b963e7911bf748
Jeremy-Poppe <jeremy.poppe@outlook.com>
2023-09-19 19:50:27 -0500

    file processor2

--- fileprocessor2.py
+++ fileprocessor2.py
@@ -1,12 +1,14 @@
 import sys
 
-for line in Lines:
-    user = line.split(":")
-    if("#" in user[0]):
-        print()
+for line in sys.stdin:
+    if '#' == line.rstrip:
+        break
     else:
-        print("The user " + user[0] + " has a password of " + user[1] + " and has a userid " + user[2] + " and groupid " + user[3])
+        print("The user " + line.find(":") + " has a password of " + line.find(":") + " and has a userid " + line.find(":") + " and groupid " + line.find(":"))
 
+    
+    
+    
 
 
 
